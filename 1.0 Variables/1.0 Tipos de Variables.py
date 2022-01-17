74455417892c9a06e334f677beeb17aa8d1de72e
LeonardoArroba <86578215+LeonardoArroba@users.noreply.github.com>
2022-01-16 22:37:36 -0500

    Add files via upload

---
+++ 1.0 Variables/1.0 Tipos de Variables.py
@@ -0,0 +1,17 @@
+# Variables
+a = 15
+print(a)
+a = 390
+print(a)
+a = 2 * 189 - 45
+print(a)
+a = 2 * (189-45)
+print(a)
+
+b = 2* a
+print(b)
+print(a)
+a = a - 100
+print(a)
+a -= 100
+print(a)
---
+++ 1.0 Variables/1.1 Ejercicios de vaiable.py
@@ -0,0 +1,34 @@
+# Ejercicios de variables
+# Primer Ejercicio
+X = 46
+X = 15
+x = 30
+print(X)
+
+# Segundo Ejercicio
+X = 10 * 2
+X-=5
+print(X)
+
+# Tercer Ejercicio
+X = 10 - 2
+10 + 2
+print(X)
+
+# Cuarto Ejercicio
+Y = 3 * (4 + 2)
+X = Y + 2
+Z = 5
+X = Y - Z
+print(X)
+
+# Quinto Ejercicio
+X = 25
+X + 10
+print(X)
+
+# Sexto Ejercicio
+X = 3
+Y = X + 6
+X = Y - 1
+print(X)
