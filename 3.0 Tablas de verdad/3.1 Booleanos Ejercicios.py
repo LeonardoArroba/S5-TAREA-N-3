82794cf6e35271d5a9c1baef73ee0bf9c59ad7bf
LeonardoArroba <86578215+LeonardoArroba@users.noreply.github.com>
2022-01-16 22:40:34 -0500

    Add files via upload

---
+++ 3.0 Tablas de verdad/3.1 Booleanos Ejercicios.py
@@ -0,0 +1,16 @@
+# ¿Qué resultado (True / False) dan las siguientes operaciones?
+print(not True)
+print(not(1 + 2!=3))
+x =(len("jugar")>5) and (len("jugar")<10)
+print(x)
+print("alto" [2] == "t" and x)
+print(842913 != 3 and len("cafe") == 3)
+print(0!= 0 or "a" < "y")
+print(True or int("50") >= 50)
+edad = 20
+a = not(x) or edad % 2 == 0
+print(a)
+es_cliente = False
+b = not(es_cliente and not (edad < 18))
+print(b)
+
